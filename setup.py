fa833ecfebdaa2f7e8dd497323511931109fd724
Gopala Krishna Koduri <gopala.koduri@gmail.com>
2024-05-03 12:24:03 +0530

    renaming repo to pyrestcountries

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="restcountries",
+    name="pyrestcountries",
     version="2024.04.29",
     description="Pythonic wrapper around data served by restcountries API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Gopala Krishna Koduri",
     author_email="gopal@riyazapp.com",
-    url="https://github.com/musicmuni/restcountries",
+    url="https://github.com/musicmuni/pyrestcountries",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["requests", "appdirs", "diskcache", "thefuzz"],
@@ -26,9 +26,9 @@ setup(
     ],
     python_requires=">=3.7",
     project_urls={
-        "Documentation": "https://github.com/musicmuni/restcountries",
-        "Source": "https://github.com/musicmuni/restcountries",
-        "Tracker": "https://github.com/musicmuni/restcountries/issues",
+        "Documentation": "https://github.com/musicmuni/pyrestcountries",
+        "Source": "https://github.com/musicmuni/pyrestcountries",
+        "Tracker": "https://github.com/musicmuni/pyrestcountries/issues",
         "Connect w/ Author": "https://linkedin.com/in/gopalkoduri",
         "Riyaz - Learn to sing": "https://riyazapp.com",
     },
